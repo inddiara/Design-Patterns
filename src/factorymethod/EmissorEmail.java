5c2831e13d103f48e461e8f1c897b2681ca87ef2
Indiara Duarte <indiara96d@gmail.com>
2018-10-02 16:12:16 -0300

    Factory method example

---
+++ src/factorymethod/EmissorEmail.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class EmissorEmail implements Emissor {
+	public void envia(String message) {
+		System.out.println("Enviando por Email a mensagem:");	
+		System.out.println(message);
+	}
+
+}
